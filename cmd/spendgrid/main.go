14bc4274b173db287071c7898995a55b8a158a2a
aligundogdu <ali@vuska.net>
2026-02-07 22:04:17 +0300

    chore: bump version to v0.2.5

--- cmd/spendgrid/main.go
+++ cmd/spendgrid/main.go
@@ -13,7 +13,7 @@ import (
 )
 
 // version is set during build using -ldflags
-var version = "v0.2.4"
+var version = "v0.2.5"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
